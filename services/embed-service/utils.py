732f1abccc3e82137150c72c27454340147b34e5
Ali Osman <alimoamen100@gmail.com>
2026-01-30 09:01:22 -0800

    Use the bm25 sparse vector generator in the embed-service

--- services/embed-service/utils.py
+++
@@ -1,8 +0,0 @@
-def convert_to_milvus_sparse_format(sparse_vectors):
-    if not sparse_vectors:
-        return None
-    milvus_sparse_vectors = []
-    for vector in sparse_vectors:
-        sparse_dict = {i: val for i, val in enumerate(vector) if val != 0.0}
-        milvus_sparse_vectors.append(sparse_dict)
-    return milvus_sparse_vectors
